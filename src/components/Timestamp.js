65ef16195783e115c6256c1d8560cc397e595154
monteiro74 <monteiro74@protonmail.com>
2021-09-20 14:12:47 +0100

    done

---
+++ src/components/Timestamp.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Timestamp({time}) {
+    return (
+        <div>
+            <span className="timestamp">{time}</span>
+        </div>
+    )
+}
